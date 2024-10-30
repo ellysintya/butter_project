87ec0c0fe4f2b24eb33ca9620d61acec0b6eaa43
elly <221110139@students.mikroskil.ac.id>
2024-10-30 19:58:06 +0700

    ini model dari data base

---
+++ lib/data base/model.dart
@@ -0,0 +1,14 @@
+class MdKaryawan {
+  int? id;
+  String nama;
+  String pass;
+  int no_telp;
+  String email;
+
+  MdKaryawan(
+      {this.id,
+      required this.nama,
+      required this.pass,
+      required this.no_telp,
+      required this.email});
+}
